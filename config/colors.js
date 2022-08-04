b82c4f925a3b6e2be3227a80490df92db47e6f00
Yuyu S <swsalim@gmail.com>
2022-08-04 14:11:03 +0800

    move colors array

--- config/colors.js
+++ config/colors.js
@@ -9,3 +9,27 @@ export const colors = [
   'rose',
   'indigo',
 ];
+
+export const containerClasses = [
+  'border-red-300 bg-red-100 text-red-900',
+  'border-orange-300 bg-orange-100 text-orange-900',
+  'border-lime-300 bg-lime-100 text-lime-900',
+  'border-emerald-300 bg-emerald-100 text-emerald-900',
+  'border-sky-300 bg-sky-100 text-sky-900',
+  'border-cyan-300 bg-cyan-100 text-cyan-900',
+  'border-purple-300 bg-purple-100 text-purple-900',
+  'border-rose-300 bg-rose-100 text-rose-900',
+  'border-indigo-300 bg-indigo-100 text-indigo-900',
+];
+
+export const blobClasses = [
+  'fill-red-50',
+  'fill-orange-50',
+  'fill-lime-50',
+  'fill-emerald-50',
+  'fill-sky-50',
+  'fill-cyan-50',
+  'fill-purple-50',
+  'fill-rose-50',
+  'fill-indigo-50',
+];
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,10 @@
-import classNames from 'classnames';
 import { farewellsChetan as farewells } from '@config/farewells';
-import { colors } from '@config/colors';
+import { colors, containerClasses, blobClasses } from '@config/colors';
 import Wrapper from '@layouts/Wrapper';
 import Blob from '@components/Blob';
 import Card from '@components/Card';
 
 export default function Farewell() {
-  const randomColorIndex = Math.floor(Math.random() * colors.length) + 1;
-  const containerClasses = [
-    'border-red-300 bg-red-100 text-red-900',
-    'border-orange-300 bg-orange-100 text-orange-900',
-    'border-lime-300 bg-lime-100 text-lime-900',
-    'border-emerald-300 bg-emerald-100 text-emerald-900',
-    'border-sky-300 bg-sky-100 text-sky-900',
-    'border-cyan-300 bg-cyan-100 text-cyan-900',
-    'border-purple-300 bg-purple-100 text-purple-900',
-    'border-rose-300 bg-rose-100 text-rose-900',
-    'border-indigo-300 bg-indigo-100 text-indigo-900',
-  ];
-
-  const blobClasses = [
-    'fill-red-50',
-    'fill-orange-50',
-    'fill-lime-50',
-    'fill-emerald-50',
-    'fill-sky-50',
-    'fill-cyan-50',
-    'fill-purple-50',
-    'fill-rose-50',
-    'fill-indigo-50',
-  ];
-
   const getRandomStyles = () => {
     const randomColorIndex = Math.floor(Math.random() * colors.length - 1) + 1;
 
